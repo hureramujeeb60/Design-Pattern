3fc8dd5834e66744679c4501a05f71383eb3fe0a
hureramujeeb60 <hureram8@gmail.com>
2024-07-31 00:20:36 +0500

    Create ExampleFactory.java

---
+++ Factory/ExampleFactory.java
@@ -0,0 +1,12 @@
+package Factory;
+
+/* Factory Design pattern is also comes under the creational design pattern as it also focuses on the creation of the object.
+Factory design pattern says to define an interface for creating the object and let the subclasses decide which class to instantiate
+*/
+
+public class ExampleFactory {
+    public static void main(String[] args) {
+
+    }
+
+}
